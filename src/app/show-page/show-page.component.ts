7789266c4bf4bd07a45e25e129be78e8816898a4
Isaac <isaac_231@hotmail.com>
2020-09-29 17:34:47 -0300

    nuevos-Modulos-agregados

---
+++ src/app/show-page/show-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-page',
+  templateUrl: './show-page.component.html',
+  styleUrls: ['./show-page.component.css']
+})
+export class ShowPageComponent{
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
