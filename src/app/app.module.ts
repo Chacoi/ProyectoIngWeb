3959da546edac6112411d8970b14402c6ca66613
Isaac <isaac_231@hotmail.com>
2020-09-29 19:19:06 -0300

    Colorizando-index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { ShowPageComponent } from './show-page/show-page.component';
+import { IndexPageComponent } from './index-page/index-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    ShowPageComponent,
+    IndexPageComponent
   ],
   imports: [
     BrowserModule,
