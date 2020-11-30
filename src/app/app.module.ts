173a00d6157cf8f9936156f98e07cde9c6f3e19c
Axel <axelne09@gmail.com>
2020-11-30 02:05:35 -0300

    Verificación de correo, sin terminar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 //import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { FormularioComponent } from './formulario/formulario.component';
-import { Formulario2Component } from './formulario2/formulario2.component'; // <-- NgModel lives here
+import { Formulario2Component } from './formulario2/formulario2.component';
+import { SendEmailComponent } from './auth/send-email/send-email.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { Formulario2Component } from './formulario2/formulario2.component'; // <
     DashboardComponent,
     FormularioComponent,
     Formulario2Component,
+    SendEmailComponent,
     
   ],
   imports: [
